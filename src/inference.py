3c2868ee6f4bc5959071509fa509ee38543d1426
Ivan-Cepeda <abog.ivancepeda@gmail.com>
2026-03-01 20:08:16 -0400

    fix: pin de numpy a 1.26.4 para evitar error de compatibilidad binaria

--- src/inference.py
+++ src/inference.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import pickle
-#Función de predecir -
+#Función de predecir -_
 def predict_risk(data: dict):
     with open("model/health_risk_model.pkl", "rb") as f:
         model = pickle.load(f)
